5b726497e7f03c76c318ac483761254a1c3d83b9
mhans19 <mdhansen19@gmail.com>
2020-09-08 21:22:59 -0600

    Added config vars for heroku

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI =  `mongodb+srv://mhans19:${process.env.DB_PASS}@cluster0.rg2ia.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 
@@ -16,10 +17,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget-tracker', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 // routes
